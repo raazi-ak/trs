2e15eaf6346fd3ec4d3e5b6e02d86f975b6e28b8
Raazi Faisal <raazi.faisal@gmail.com>
2024-05-02 13:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ app.config['SQLALCHEMY_DATABASE_URI']=\
             'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SERVER_NAME'] = '127.0.0.1:5000'
-app.config['SECRET_KEY'] = 'gnKRCJ3jmXqHiIcKHrGkByfReiyQTVP7'
-app.config['JWT_SECRET_KEY'] = 'gnKRCJ3jmXqHiIcKHrGkByfReiyQTVP7'
+app.config['SECRET_KEY'] = '$SECRET_KEY'
+app.config['JWT_SECRET_KEY'] = '$JWT_SECRET_KEY'
 SWAGGER_URL="/swagger"
 API_URL="/static/swagger.yaml"
 
